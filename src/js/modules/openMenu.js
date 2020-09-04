70b767e62382b9b908b5151604c3aad8db1f8c7f
Yevgen <yevgen.code@gmail.com>
2020-09-04 12:28:40 +0300

    добавлена функция убирающаа прокрутку при вызове меню

--- src/js/modules/openMenu.js
+++ src/js/modules/openMenu.js
@@ -2,16 +2,19 @@ function openMenu(trigerSelector, contentSelector, closeSelector, overlaySelecto
     const triger = document.querySelector(trigerSelector),
           menu = document.querySelector(contentSelector),
           closeMenu = document.querySelector(closeSelector),
-          overlay = document.querySelector(overlaySelector);
+          overlay = document.querySelector(overlaySelector),
+          scroll = calcScroll();
 
     function close() {
         menu.classList.remove('active');
         document.body.style.overflow = '';
+        document.body.style.marginRight = '0px';
     }
 
     triger.addEventListener('click', () => {
         menu.classList.add('active');
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
     });
 
     closeMenu.addEventListener('click', close);
@@ -27,6 +30,22 @@ function openMenu(trigerSelector, contentSelector, closeSelector, overlaySelecto
             close();
         }
     });
+
+    //убираем прокрутку при открытии меню
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.cssText = 'width: 50px; height: 50px; overflow-y: scroll; visibility: hidden';
+
+        document.body.appendChild(div);
+
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+
+        div.remove();
+
+        return scrollWidth;
+    }
 }
 
 export default openMenu;
